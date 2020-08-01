596b35cb9c9b4fe8ca1a161393886d6298040020
darexu <darexr.u@mail.ru>
2020-08-01 07:04:26 +0300

    I18n in errors

--- app/models/event.rb
+++ app/models/event.rb
@@ -6,13 +6,13 @@ class Event < ApplicationRecord
 
   validates :title, presence: true, length: {maximum: 255}
   validates :address, presence: true
-  validates :datetime, presence: true
-  validate :datetime_in_future?, on: :update
+  validates :datetime, presence: true, format: { with: /\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}/ }
+  validate :datetime_in_future?, on: [:update, :create]
 
   private
   def datetime_in_future?
-    if datetime < Date.today
-      errors.add(:expiration_date, "can't be in the past")
+    if datetime < Time.now
+      errors.add(:datetime, I18n.t('errors.messages.not_saved.wrong_date_format'))
     end
   end
 end
