e1d5b9ec7cea4e98f35cb18263a52da00676023e
darexu <darexr.u@mail.ru>
2020-09-03 05:18:39 +0300

    added scope to comments created at desc

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -6,6 +6,8 @@ class Comment < ApplicationRecord
   # поле должно быть, только если не выполняется user.present? (у объекта на задан юзер)
   validates :user_name, presence: true, unless: -> { user.present? }
 
+  scope :sort_by_newest, -> { order(created_at: :desc) }
+
   def user_name
     user&.name.presence || super
   end
