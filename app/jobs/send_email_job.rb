fdc73ef7060173991d4e39bf6370fd8d18725bba
darexu <darexr.u@mail.ru>
2020-10-23 04:52:47 +0300

    update send_email_job

--- app/jobs/send_email_job.rb
+++ app/jobs/send_email_job.rb
@@ -4,9 +4,9 @@ class SendEmailJob < ApplicationJob
   def perform(model)
     all_emails = (model.event.subscriptions.map(&:user_email) + [model.event.user.email] - [model.user&.email]).uniq
 
-    if model == Comment
+    if model.is_a? Comment
       all_emails.each { |mail| EventMailer.comment(model.event, model, mail).deliver_later }
-    elsif model == Photo
+    elsif model.is_a? Photo
       all_emails.each { |mail|  EventMailer.photo(model.event, model, mail).deliver_later }
     end
   end
--- config/deploy.rb
+++ config/deploy.rb
@@ -6,7 +6,7 @@ set :repo_url, 'git@github.com:darexu/bbq.git'
 
 set :deploy_to, '/home/deploy/apps/bbq'
 
-append :linked_files, 'config/database.yml', 'config/credentials.yml.enc', '.env'
+append :linked_files, 'config/database.yml', 'config/credentials.yml.enc', '.env.production'
 append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads'
 
 after 'deploy:restart', 'resque:restart'
