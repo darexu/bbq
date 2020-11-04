02b57b1a1f3ca41f8fca410a63f7e7135914a69b
darexu <darexr.u@mail.ru>
2020-11-04 19:35:48 +0200

    user_spec update

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,19 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  describe '.find_for_facebook_oauth' do
+  describe '.find_for_oauth' do
     let(:access_token) do
       double(
         :access_token,
         provider: 'facebook',
-        info: double(email: 'install.vv@gmail.com'),
+        info: double(email: 'install.vv@gmail.com', name: 'Vadim'),
         extra: double(raw_info: double(id: '10213477015136709'))
       )
     end
 
     context 'when user is not found' do
       it 'returns newly created user' do
-        user = User.find_for_facebook_oauth(access_token)
+        user = User.find_for_oauth(access_token)
 
         expect(user).to be_persisted
         expect(user.email).to eq 'install.vv@gmail.com'
@@ -25,7 +25,7 @@ RSpec.describe User, type: :model do
       let!(:some_other_user) { create(:user) }
 
       it 'returns this user' do
-        expect(User.find_for_facebook_oauth(access_token)).to eq existing_user
+        expect(User.find_for_oauth(access_token)).to eq existing_user
       end
     end
 
@@ -37,7 +37,7 @@ RSpec.describe User, type: :model do
       let!(:some_other_uer) { create(:user) }
 
       it 'returns this user' do
-        expect(User.find_for_facebook_oauth(access_token)).to eq existing_user
+        expect(User.find_for_oauth(access_token)).to eq existing_user
       end
     end
   end
